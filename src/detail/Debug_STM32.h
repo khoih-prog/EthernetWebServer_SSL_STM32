934dac2f54ac1993f9c17be45fd382281779989e
Khoi Hoang <57012152+khoih-prog@users.noreply.github.com>
2020-11-18 03:49:16 -0500

    v1.1.1
    ### Release v1.1.1
    
    1. Permit sites with "Chain could not be linked to a trust anchor" such as mqtt.thingstream.io.
    2. Add example MQTTS_ThingStream to demonstrate new feature

--- src/detail/Debug_STM32.h
+++ src/detail/Debug_STM32.h
@@ -1,9 +1,7 @@
 /****************************************************************************************************************************
   Debug_STM32.h - Dead simple Ethernet web-server.
-  For STM32 with built-in Ethernet LAN8742A (Nucleo-144, DISCOVERY, etc) or W5x00/ENC28J60 shield/module
-
   For STM32F/L/H/G/WB/MP1 with built-in Ethernet LAN8742A (Nucleo-144, DISCOVERY, etc) or W5x00/ENC28J60 shield/module
-
+  
   EthernetWebServer_SSL_STM32 is a library for STM32 using the Ethernet shields to run WebServer and Client with/without SSL
 
   Use SSLClient Library code from https://github.com/OPEnSLab-OSU/SSLClient
@@ -11,12 +9,13 @@
   Built by Khoi Hoang https://github.com/khoih-prog/EthernetWebServer_SSL_STM32
   Licensed under MIT license
        
-  Version: 1.1.0
+  Version: 1.1.1
 
   Version Modified By   Date      Comments
   ------- -----------  ---------- -----------
   1.1.0   K Hoang      14/11/2020 Initial coding for STM32F/L/H/G/WB/MP1 to support Ethernet shields using SSL. Supporting BI LAN8742A, 
-                                  W5x00 using Ethernetx, ENC28J60 using EthernetENC and UIPEthernet libraries
+                                  W5x00 using Ethernetx, ENC28J60 using EthernetENC and UIPEthernet libraries   
+  1.1.1   K Hoang      18/11/2020 Permit sites with "Chain could not be linked to a trust anchor" such as ThingStream
  *****************************************************************************************************************************/
 
 #ifndef EthernetWebServer_STM32_Debug_H
--- src/inner.h
+++ src/inner.h
@@ -1,19 +1,22 @@
 /****************************************************************************************************************************
   inner.h
   
-  EthernetWebServer_SSL is a library for the Ethernet shields to run WebServer and Client with/without SSL
+  For STM32F/L/H/G/WB/MP1 with built-in Ethernet LAN8742A (Nucleo-144, DISCOVERY, etc) or W5x00/ENC28J60 shield/module
+  
+  EthernetWebServer_SSL_STM32 is a library for STM32 using the Ethernet shields to run WebServer and Client with/without SSL
 
   Use SSLClient Library code from https://github.com/OPEnSLab-OSU/SSLClient
   
-  Built by Khoi Hoang https://github.com/khoih-prog/EthernetWebServer_SSL
+  Built by Khoi Hoang https://github.com/khoih-prog/EthernetWebServer_SSL_STM32
   Licensed under MIT license
        
-  Version: 1.1.0
+  Version: 1.1.1
 
   Version Modified By   Date      Comments
   ------- -----------  ---------- -----------
-  1.1.0   K Hoang      14/11/2020 Initial coding for SAMD21/SAMD51, nRF52, SAM DUE to support Ethernet shields using SSL.
-                                  Supporting W5x00 using Ethernetx, ENC28J60 using EthernetENC and UIPEthernet libraries
+  1.1.0   K Hoang      14/11/2020 Initial coding for STM32F/L/H/G/WB/MP1 to support Ethernet shields using SSL. Supporting BI LAN8742A, 
+                                  W5x00 using Ethernetx, ENC28J60 using EthernetENC and UIPEthernet libraries   
+  1.1.1   K Hoang      18/11/2020 Permit sites with "Chain could not be linked to a trust anchor" such as ThingStream
  *****************************************************************************************************************************/
 
 #include "SSLClient/inner.h"
