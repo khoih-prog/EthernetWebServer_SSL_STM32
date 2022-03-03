07000c7baf5228b5de66e68296f3fb038314a3c4
Khoi Hoang <57012152+khoih-prog@users.noreply.github.com>
2022-03-02 22:22:37 -0500

    v1.4.3 to fix decoding error bug
    ### Releases v1.4.3
    
    1. Fix decoding error bug when using special `&` in data fields. Check [Decoding Error. two times called urlDecode in Parsing-impl.h. #17](https://github.com/khoih-prog/WiFiWebServer/issues/17)

--- src/Ethernet_HTTPClient/Ethernet_HttpClient.h
+++ src/Ethernet_HTTPClient/Ethernet_HttpClient.h
@@ -11,21 +11,17 @@
        
   Licensed under MIT license
   
-  Version: 1.4.2
+  Version: 1.4.3
 
   Version Modified By   Date      Comments
   ------- -----------  ---------- -----------
   1.1.0   K Hoang      14/11/2020 Initial coding for STM32F/L/H/G/WB/MP1 to support Ethernet shields using SSL. Supporting BI LAN8742A, 
                                   W5x00 using Ethernetx, ENC28J60 using EthernetENC and UIPEthernet libraries   
-  1.1.1   K Hoang      18/11/2020 Permit sites with "Chain could not be linked to a trust anchor" such as ThingStream
-  1.1.2   K Hoang      19/11/2020 Add SSL debug feature. Enhance examples.
-  1.2.0   K Hoang      20/11/2020 Add basic HTTP and WebSockets Client by merging ArduinoHttpClient
-  1.2.1   K Hoang      26/12/2020 Suppress all possible compiler warnings
-  1.3.0   K Hoang      11/04/2021 Add support to LAN8720 using STM32F4 or STM32F7
-  1.3.1   K Hoang      04/10/2021 Change option for PIO `lib_compat_mode` from default `soft` to `strict`. Update Packages Patches
+  ...
   1.4.0   K Hoang      25/12/2021 Reduce usage of Arduino String with std::string. Fix bug
   1.4.1   K Hoang      27/12/2021 Fix wrong http status header bug and authenticate issue caused by libb64
   1.4.2   K Hoang      11/01/2022 Fix libb64 fallthrough compile warning
+  1.4.3   K Hoang      02/03/2022 Fix decoding error bug
  *************************************************************************************************************************************/
 
 // Class to simplify HTTP fetching on Arduino
