67a751e234922bf8d2b6eede0bfbd5847915989a
Khoi Hoang <57012152+khoih-prog@users.noreply.github.com>
2022-11-23 00:55:44 -0500

    Update `README.md` and use `allman` astyle

--- examples/HTTPClient/CustomHeader/CustomHeader.ino
+++ examples/HTTPClient/CustomHeader/CustomHeader.ino
@@ -2,11 +2,11 @@
   CustomHeader.ino - Simple Arduino web server sample for Ethernet shield
 
   For STM32F/L/H/G/WB/MP1 with built-in Ethernet LAN8742A (Nucleo-144, DISCOVERY, etc) or W5x00/ENC28J60 shield/module
-  
+
   EthernetWebServer_SSL_STM32 is a library for STM32 using the Ethernet shields to run WebServer and Client with/without SSL
 
   Use SSLClient Library code from https://github.com/OPEnSLab-OSU/SSLClient
-  
+
   Built by Khoi Hoang https://github.com/khoih-prog/EthernetWebServer_SSL_STM32
 
   Custom request header example for the ArduinoHttpClient
@@ -30,6 +30,7 @@ EthernetHttpClient  httpClient(client, serverAddress, port);
 void setup()
 {
   Serial.begin(115200);
+
   while (!Serial && millis() < 5000);
 
   Serial.print("\nStarting BasicAuthGet on " + String(BOARD_NAME));
@@ -38,40 +39,40 @@ void setup()
 #if USE_ETHERNET_GENERIC
   Serial.println(ETHERNET_GENERIC_VERSION);
 #endif
-  
+
   Serial.println(ETHERNET_WEBSERVER_SSL_STM32_VERSION);
 
 #if !(USE_BUILTIN_ETHERNET)
-  #if (USING_SPI2)
-    #if defined(CUR_PIN_MISO)
-      ET_LOGWARN(F("Default SPI pinout:"));
-      ET_LOGWARN1(F("MOSI:"), CUR_PIN_MOSI);
-      ET_LOGWARN1(F("MISO:"), CUR_PIN_MISO);
-      ET_LOGWARN1(F("SCK:"),  CUR_PIN_SCK);
-      ET_LOGWARN1(F("SS:"),   CUR_PIN_SS);
-      ET_LOGWARN(F("========================="));
-    #endif
-  #else
-    ET_LOGWARN(F("Default SPI pinout:"));
-    ET_LOGWARN1(F("MOSI:"), MOSI);
-    ET_LOGWARN1(F("MISO:"), MISO);
-    ET_LOGWARN1(F("SCK:"),  SCK);
-    ET_LOGWARN1(F("SS:"),   SS);
-    ET_LOGWARN(F("========================="));
-  #endif
+#if (USING_SPI2)
+#if defined(CUR_PIN_MISO)
+  ET_LOGWARN(F("Default SPI pinout:"));
+  ET_LOGWARN1(F("MOSI:"), CUR_PIN_MOSI);
+  ET_LOGWARN1(F("MISO:"), CUR_PIN_MISO);
+  ET_LOGWARN1(F("SCK:"),  CUR_PIN_SCK);
+  ET_LOGWARN1(F("SS:"),   CUR_PIN_SS);
+  ET_LOGWARN(F("========================="));
+#endif
+#else
+  ET_LOGWARN(F("Default SPI pinout:"));
+  ET_LOGWARN1(F("MOSI:"), MOSI);
+  ET_LOGWARN1(F("MISO:"), MISO);
+  ET_LOGWARN1(F("SCK:"),  SCK);
+  ET_LOGWARN1(F("SS:"),   SS);
+  ET_LOGWARN(F("========================="));
+#endif
 #endif
 
 #if !(USE_BUILTIN_ETHERNET || USE_UIP_ETHERNET)
   // For other boards, to change if necessary
-  #if ( USE_ETHERNET_GENERIC || USE_ETHERNET_ENC )
+#if ( USE_ETHERNET_GENERIC || USE_ETHERNET_ENC )
   Ethernet.init (USE_THIS_SS_PIN);
 
-  #elif USE_CUSTOM_ETHERNET
+#elif USE_CUSTOM_ETHERNET
   // You have to add initialization for your Custom Ethernet here
   // This is just an example to setCSPin to USE_THIS_SS_PIN, and can be not correct and enough
   //Ethernet.init(USE_THIS_SS_PIN);
 
-  #endif  //( ( USE_ETHERNET_GENERIC || USE_ETHERNET_ENC )
+#endif  //( ( USE_ETHERNET_GENERIC || USE_ETHERNET_ENC )
 #endif
 
   // start the ethernet connection and the server:
@@ -85,11 +86,11 @@ void setup()
   Serial.println(Ethernet.localIP());
 }
 
-void loop() 
+void loop()
 {
   Serial.println("Making GET request");
   httpClient.beginRequest();
-  
+
   httpClient.get("/");
   httpClient.sendHeader("X-CUSTOM-HEADER", "custom_value");
   httpClient.endRequest();
